16e23682a7211c37bd0d5cc2f056f7fb5279260c
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-15 23:01:32 +0300

    title scene. add two buttons. play & credits. createBtn function & TitleScene class

---
+++ src/Scenes/TitleScene.js
@@ -0,0 +1,61 @@
+import Phaser from 'phaser';
+import config from '../Config/config';
+import Button from '../Objects/Button';
+
+export default class TitleScene extends Phaser.Scene {
+  constructor() {
+    super('Title');
+  }
+
+  preload() {
+    this.load.image('gameoverBg', 'assets/content/bg.jpg');
+  }
+
+  create() {
+    this.add.image(380, 299, 'gameoverBg');
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: "SPIDER ATTACK",
+      style: {
+        font: '800 50px monospace',
+        fill: 'black',
+        stroke: 'red',
+        strokeThickness: 8,
+        shadow: 'black',
+      },
+    });
+    loadingText.setOrigin(0.5, 3.5);
+
+
+    this.gameButton = new Button(this, config.width / 2, config.height / 2 - 100, 'blueButton1', 'blueButton2', 'Play', 'Game');
+
+    this.creditsButton = new Button(this, config.width / 2, config.height / 2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
+
+    this.model = this.sys.game.globals.model;
+    if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
+      this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
+      this.bgMusic.play();
+      this.model.bgMusicPlaying = true;
+      this.sys.game.globals.bgMusic = this.bgMusic;
+    }
+  }
+
+  centerButton(gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(
+        config.width / 2, config.height / 2 - offset * 100, config.width, config.height,
+      ),
+    );
+  }
+
+  centerButtonText(gameText, gameButton) {
+    Phaser.Display.Align.In.Center(
+      gameText,
+      gameButton,
+    );
+  }
+}
