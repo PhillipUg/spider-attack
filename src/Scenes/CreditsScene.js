5b24ba05c7fa9d385a6472b36f1f99c3c260e6d7
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-16 13:24:41 +0300

    added credits scene. Just for because i could haha

---
+++ src/Scenes/CreditsScene.js
@@ -0,0 +1,49 @@
+import Phaser from 'phaser';
+import config from '../Config/config';
+
+export default class CreditsScene extends Phaser.Scene {
+  constructor() {
+    super('Credits');
+  }
+
+  create() {
+    this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Phillip Musiime \n \n Github: PhillipUg \n \n Twitter: @Phillip_Ug \n \n Email: phillipmusiime@gmail.com', { fontSize: 16, fill: '#fff' });
+    this.zone = this.add.zone(config.width / 2, config.height / 2, config.width, config.height);
+
+    Phaser.Display.Align.In.Center(
+      this.creditsText,
+      this.zone,
+    );
+
+    Phaser.Display.Align.In.Center(
+      this.madeByText,
+      this.zone,
+    );
+
+    this.madeByText.setY(1000);
+
+    this.creditsTween = this.tweens.add({
+      targets: this.creditsText,
+      y: -100,
+      ease: 'Power1',
+      duration: 3000,
+      delay: 500,
+      onComplete() {
+        this.destroy;
+      },
+    });
+
+    this.madeByTween = this.tweens.add({
+      targets: this.madeByText,
+      y: -200,
+      ease: 'Power1',
+      duration: 10000,
+      delay: 500,
+      onComplete: function () {
+        this.madeByTween.destroy;
+        this.scene.start('Title');
+      }.bind(this),
+    });
+  }
+}
